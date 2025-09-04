0e2ec624c02613d347f6d3fb143f88bbb37883ac
Mohamed Nouar <mohnouar57@gmail.com>
2025-09-04 11:38:36 +0100

    Fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
-import Projects from "./components/projects"
+import Projects from "./components/Projects"
 import Testimonials from "./components/Testimonials"
 
 
